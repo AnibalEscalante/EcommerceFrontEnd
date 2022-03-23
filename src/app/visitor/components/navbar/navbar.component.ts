4f3bc882ee7638726f26c23ede22c184c88c30f1
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2022-03-23 16:50:35 -0300

    add navbar

--- src/app/visitor/components/navbar/navbar.component.ts
+++ src/app/visitor/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.less']
 })
 export class NavbarComponent implements OnInit {
+  
+  public categoryList: FormControl;
+  public options: string[];
+  public filteredOptions: Observable<string[]> | null;
 
-  constructor() { }
+  constructor() {
+    this.categoryList = new FormControl();
+    this.options = ['celulares', 'audifonos', 'monitores'];
+    this.filteredOptions = null;
+  }
 
   ngOnInit(): void {
+    this.filteredOptions = this.categoryList.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLocaleLowerCase().includes(filterValue));
   }
 
 }
