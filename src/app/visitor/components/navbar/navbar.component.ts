b91cbdf2cc431279f0e66295be966e01e231935a
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2022-03-24 16:41:45 -0300

    change styles navbar visitor

--- src/app/visitor/components/navbar/navbar.component.ts
+++ src/app/visitor/components/navbar/navbar.component.ts
@@ -8,27 +8,11 @@ import { map, Observable, startWith } from 'rxjs';
   styleUrls: ['./navbar.component.less']
 })
 export class NavbarComponent implements OnInit {
-  
-  public categoryList: FormControl;
-  public options: string[];
-  public filteredOptions: Observable<string[]> | null;
 
   constructor() {
-    this.categoryList = new FormControl();
-    this.options = ['celulares', 'audifonos', 'monitores'];
-    this.filteredOptions = null;
   }
 
   ngOnInit(): void {
-    this.filteredOptions = this.categoryList.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value))
-    );
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLocaleLowerCase().includes(filterValue));
   }
 
 }
