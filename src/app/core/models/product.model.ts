7b0144aa0e9c136aad5518f59954799cef37cdee
Anibal <anibal78e@gmail.com>
2022-04-02 18:47:33 -0300

    category screen model interface

---
+++ src/app/core/models/product.model.ts
@@ -0,0 +1,5 @@
+export interface Product{
+    brand: string;
+    description: string;
+    price: string;
+}
--- src/app/user/components/category/category.component.ts
+++ src/app/user/components/category/category.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+interface Product{
+  brand: string;
+  description: string;
+  price: string;
+}
+const PRODUCT_DATA: Product[] = [
+  {
+    brand: 'Sony',
+    description:'AUDIFONOS SONY BT WH-CH510 AZUL',
+    price: '$150.000'
+  },
+  {
+    brand: 'Panasonic',
+    description:'AUDIFONOS Panasonic BT WH-CH510 AZUL',
+    price: '$100.000'
+  },
+  {
+    brand: 'Motorola',
+    description:'AUDIFONOS Motorola BT WH-CH510 AZUL',
+    price: '$150.000'
+  },
+  {
+    brand: 'Apple',
+    description:'AUDIFONOS Apple BT WH-CH510 AZUL',
+    price: '$1.500.000'
+  },
+  {
+    brand: 'Sony',
+    description:'AUDIFONOS SONY BT WH-CH510 AZUL',
+    price: '$150.000'
+  },
+];
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.less']
 })
 export class CategoryComponent implements OnInit {
-
+  @Input() products!: Product;
   constructor() { }
 
   ngOnInit(): void {
