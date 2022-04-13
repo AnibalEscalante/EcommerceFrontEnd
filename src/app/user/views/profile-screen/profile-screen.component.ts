9d63a96976ed7015f2a624e5642bf6d5fae35d9c
Anibal <anibal78e@gmail.com>
2022-04-13 17:25:44 -0400

    profile add

--- src/app/user/views/profile-screen/profile-screen.component.ts
+++ src/app/user/views/profile-screen/profile-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-screen',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-screen.component.less']
 })
 export class ProfileScreenComponent implements OnInit {
-
+  value = 'Clear me';
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
