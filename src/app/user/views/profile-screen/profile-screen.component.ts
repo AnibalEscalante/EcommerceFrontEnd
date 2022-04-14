7f5667e602f1203da75c5d144188f16ae4f1658d
Anibal <anibal78e@gmail.com>
2022-04-14 18:21:14 -0400

    profile

--- src/app/user/views/profile-screen/profile-screen.component.ts
+++ src/app/user/views/profile-screen/profile-screen.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-screen',
@@ -8,18 +8,61 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ProfileScreenComponent implements OnInit {
   value = 'Clear me';
-  constructor() { }
+
+  public updateForm: FormGroup;
+  public hideP: boolean;
+  public hideCP: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.hideP = true;
+    this.hideCP = true;
+    this.updateForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern('[a-zA-Z]{2,32}')]],
+      lastNameP: ['', [Validators.required, Validators.pattern('[a-zA-Z]{2,32}')]],
+      lastNameM: ['', [Validators.required, Validators.pattern('[a-zA-Z]{2,32}')]],
+      run: ['', [Validators.required, Validators.pattern('[a-zA-Z]{2,32}')]],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")]],
+    });
+  }
 
   ngOnInit(): void {
   }
-  
-  email = new FormControl('', [Validators.required, Validators.email]);
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+  get name() { return this.updateForm?.get('name'); }
+  get lastNameP() { return this.updateForm?.get('lastNameP'); }
+  get lastNameM() { return this.updateForm?.get('lastNameM'); }
+  get run() { return this.updateForm?.get('run'); }
+  get email() { return this.updateForm?.get('email'); }
+  public getNameErrorMessage() {
+    if (this.name?.hasError('required')) {
+      return 'Se requiere su Nombre';
     }
+    return this.name?.invalid ? 'El dato ingresado no es valido' : '';
+  }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  public getLastNamePErrorMessage() {
+    if (this.lastNameP?.hasError('required')) {
+      return 'Se requiere su primer Apellido';
+    }
+    return this.lastNameP?.invalid ? 'El dato ingresado no es valido' : '';
+  }
+
+  public getLastNameMErrorMessage() {
+    if (this.lastNameP?.hasError('required')) {
+      return 'Se requiere su segundo Apellido';
+    }
+    return this.lastNameM?.invalid ? 'El dato ingresado no es valido' : '';
+  }
+
+  public getEmailErrorMessage() {
+    if (this.email?.hasError('required')) {
+      return 'Se requiere el email';
+    }
+    return this.email?.invalid ? 'El dato ingresado no es valido' : '';
+  }
+  onSubmit(){
+
   }
 }
