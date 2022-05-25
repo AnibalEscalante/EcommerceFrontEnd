088c1fcd9a1765728704691927af628a3b7a9bd9
Anibal <anibal78e@gmail.com>
2022-05-25 17:58:46 -0400

    category id ADD

--- src/app/shared/components/category/category.component.ts
+++ src/app/shared/components/category/category.component.ts
@@ -3,53 +3,9 @@ import { ActivatedRoute } from '@angular/router';
 import { Category } from 'src/app/core/models/category.model';
 import { Product } from 'src/app/core/models/product.model';
 import { SubCategory } from 'src/app/core/models/subCategory.model';
-import { CategoryService } from 'src/app/core/services/category/category.service';
 import { SubCategoryService } from 'src/app/core/services/subCategory/sub-category.service';
 
-/* interface Product{
-  id:string;
-  brand: string;
-  description: string;
-  price: string;
-  image: string;
-}
-const PRODUCT_DATA: Product[] = [
-  {
-    id:'1',
-    brand: 'Sony',
-    description:'AUDIFONOS SONY BT WH-CH510 AZUL',
-    price: '$150.000',
-    image: 'a1'
-  },
-  {
-    id:'2',
-    brand: 'Panasonic',
-    description:'AUDIFONOS Panasonic BT WH-CH510 AZUL',
-    price: '$100.000',
-    image: 'a2'
-  },
-  {
-    id:'3',
-    brand: 'Motorola',
-    description:'AUDIFONOS Motorola BT WH-CH510 AZUL',
-    price: '$150.000',
-    image: 'a3'
-  },
-  {
-    id:'4',
-    brand: 'Apple',
-    description:'AUDIFONOS Apple BT WH-CH510 AZUL',
-    price: '$1.500.000',
-    image: 'a4'
-  },
-  {
-    id:'5',
-    brand: 'Sony',
-    description:'AUDIFONOS SONY BT WH-CH510 AZUL',
-    price: '$150.000',
-    image: 'a5'
-  },
-]; */
+
 
 @Component({
   selector: 'app-category',
@@ -59,29 +15,28 @@ const PRODUCT_DATA: Product[] = [
 export class CategoryComponent implements OnInit {
 
   @Input() category!: Category;
-  public subCategories: SubCategory[] = [];
-  public idCategory: string;
+  public subCategory!: SubCategory;
+  public idSubCategory: string;
+  public products!: Product[];
   constructor(
     public activatedRoute: ActivatedRoute,
     public getCategoryService: SubCategoryService,
   ) { 
-    this.subCategories = [];
-    this.idCategory = "";
+    this.idSubCategory = this.activatedRoute.snapshot.params['id'];
   
   }
 
   ngOnInit(): void {
-    /* this.refresh(); */
-    /* console.log(this.category._id) */
     this.fetchSubCategory()
   }
   
   async fetchSubCategory() {
     try {
-      const response: any = await this.getCategoryService.getSubCategory(this.category._id).toPromise();
-      this.subCategories = response;
-     /*  console.log(this.category.name) */
-      /* console.log(this.subCategories) */
+      const response: any = await this.getCategoryService.getSubCategory(this.idSubCategory).toPromise();
+      this.subCategory = response.message;
+      this.products = this.subCategory.products
+      console.log(this.products);
+      
 
     }
     catch (error) {
