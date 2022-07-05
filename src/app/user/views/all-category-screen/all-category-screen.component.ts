e1ba83f0843794b4d78675862a7af034af25c0d8
Anibal <anibal78e@gmail.com>
2022-07-05 18:13:22 -0400

    CHANGE category component

--- src/app/user/views/all-category-screen/all-category-screen.component.ts
+++ src/app/user/views/all-category-screen/all-category-screen.component.ts
@@ -10,20 +10,35 @@ import { CategoryService } from 'src/app/core/services/category/category.service
 })
 export class AllCategoryScreenComponent implements OnInit {
   
-  public isSubCategory: string;
+ /*  public isSubCategory: string; */
   public products!: Product[];
   constructor(
     public activatedRoute: ActivatedRoute,
     public getCategoryService: CategoryService
   ) {
 
-    this.isSubCategory = 'allCat'
+/*     this.isSubCategory = 'allCat' */
     
   }
 
   public searchText: string = '';
   onSearchTextEntered(searchValue: string) {
     this.searchText = searchValue;
+    console.log(this.searchText);
+    this.fetchCategories();
+    
+  }
+
+  async fetchCategories(){
+    try {
+      const response: any = await this.getCategoryService.getProductCategoriesName(this.searchText).toPromise();
+      console.log(response);
+      this.products = response.message.productList
+      console.log(this.products)
+    }
+    catch (error) {
+      console.log('Algo ha salido mal');
+    }
   }
 
   public isTextActivated: boolean = true;
